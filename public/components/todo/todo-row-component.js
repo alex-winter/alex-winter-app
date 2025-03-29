607b5446c05fc1617c1625fd829d1a6419e87d92
Alex Winter <ajwinter42@gmail.com>
2025-03-29 19:32:53 +0000

    flex style refactor for todo

--- public/components/todo/todo-row-component.js
+++ public/components/todo/todo-row-component.js
@@ -24,13 +24,13 @@ export class TodoRowComponent extends Component
 
         return /*html*/`
             <div class="row flex-row p-3 gap-1">
-                <div>
+                <div class="flex-column justify-center">
                     <i class="fa-solid fa-clipboard-list"></i>
                 </div>
-                <div class="flex-2 name-container">
+                <div class="flex-2 flex-column justify-center name-container">
                     <p>${data.name}</p>
                 </div>
-                <div class="flex-1 flex-row justify-end">
+                <div class="flex-1 flex-row align-center justify-end">
                     <delete-button-component></delete-button-component>
                 </div>
             </div>
