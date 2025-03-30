e452be566ab9b3aaae5a5d7bda98246e4ba55da7
Alex Winter <ajwinter42@gmail.com>
2025-03-30 21:01:55 +0100

    move modal into its own folder

--- public/index.js
+++ public/index.js
@@ -7,7 +7,7 @@ import { DateTimeComponent } from './components/date-time-component.js'
 import { TodoComponent } from './components/todo/todo-component.js'
 import { DeleteButtonComponent } from './components/generic/delete-button-component.js'
 import { TodoRowComponent } from './components/todo/todo-row-component.js'
-import { ModalComponent } from './components/generic/modal-component.js'
+import { ModalComponent } from './components/generic/modal/modal-component.js'
 import { TodoItemEditFormComponent } from './components/todo/todo-item-edit-form-component.js'
 
 DeleteButtonComponent.load()
