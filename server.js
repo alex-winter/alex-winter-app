c7aaf069a85cfdc7b64c59521a9e42564902cfd4
Alex Winter <ajwinter42@gmail.com>
2025-04-02 19:59:01 +0100

    website

--- server.js
+++ server.js
@@ -30,8 +30,13 @@ app.post('/api/todo', (request, response) => {
   response.status(200).json({ message: "Success" })
 })
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'))
+app.get('/', (request, response) => {
+  console.log('hit')
+  response.sendFile(path.join(__dirname, 'public', 'website.html'))
+})
+
+app.get('/example/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'example.html'))
 })
 
 app.listen(PORT, () => {
